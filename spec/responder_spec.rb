c7709ad6fbaa7f860854cbfc3b0a01ea436c5b44
Paul Kehrer <paul.l.kehrer@gmail.com>
2011-12-17 20:12:57 -0600

    add basic POST tests

--- spec/responder_spec.rb
+++ spec/responder_spec.rb
@@ -60,4 +60,62 @@ describe R509::Ocsp::Responder do
         ocsp_response.basic.status[0][1].should == OpenSSL::OCSP::V_CERTSTATUS_GOOD
         ocsp_response.basic.status[0][0].serial.should == 1051177536915098490149656742929223623669143613238
     end
+    it "should return unauthorized on a POST unrelated to the configured CA" do
+        class R509::Validity::Redis::Checker
+            def check(serial)
+                raise StandardError.new("Shouldn't ever call the Checker here, since the request isn't from the configured CA")
+            end
+        end
+        der = Base64.decode64(URI.decode("MFEwTzBNMEswSTAJBgUrDgMCGgUABBQ1mI4Ww4R5LZiQ295pj4OF%2F44yyAQUyk7dWyc1Kdn27sPlU%2B%2BkwBmWHa8CEFqb7H4xpqYH6ed2G0%2BPMG4%3D"))
+        post '/', der, "CONTENT_TYPE" => "application/ocsp-request"
+        last_response.content_type.should == "application/ocsp-response"
+        last_response.should be_ok
+        ocsp_response = R509::Ocsp::Response.parse(last_response.body)
+        ocsp_response.status.should == OpenSSL::OCSP::RESPONSE_STATUS_UNAUTHORIZED
+    end
+    it "should return a valid (UNKNOWN) response on a POST request from the configured CA" do
+        class R509::Validity::Redis::Checker
+            def check(serial)
+                R509::Validity::Status.new(:status => R509::Validity::UNKNOWN)
+            end
+        end
+        der = Base64.decode64(URI.decode("MFYwVDBSMFAwTjAJBgUrDgMCGgUABBQ4ykaMB0SN9IGWx21tTHBRnmCnvQQUeXW7hDrLLN56Cb4xG0O8HCpNU1gCFQC4IG5U4zC4RYb4VQ%2B2f0zCoFCvNg%3D%3D"))
+        post '/', der, "CONTENT_TYPE" => "application/ocsp-request"
+        last_response.content_type.should == "application/ocsp-response"
+        last_response.should be_ok
+        ocsp_response = R509::Ocsp::Response.parse(last_response.body)
+        ocsp_response.status.should == OpenSSL::OCSP::RESPONSE_STATUS_SUCCESSFUL
+        ocsp_response.basic.status[0][1].should == OpenSSL::OCSP::V_CERTSTATUS_UNKNOWN
+        ocsp_response.basic.status[0][0].serial.should == 1051177536915098490149656742929223623669143613238
+    end
+    it "should return a valid (REVOKED) response on a POST request from the configured CA" do
+        class R509::Validity::Redis::Checker
+            def check(serial)
+                R509::Validity::Status.new(:status => R509::Validity::REVOKED, :revocation_time => 123, :revocation_reason => 1)
+            end
+        end
+        der = Base64.decode64(URI.decode("MFYwVDBSMFAwTjAJBgUrDgMCGgUABBQ4ykaMB0SN9IGWx21tTHBRnmCnvQQUeXW7hDrLLN56Cb4xG0O8HCpNU1gCFQC4IG5U4zC4RYb4VQ%2B2f0zCoFCvNg%3D%3D"))
+        post '/', der, "CONTENT_TYPE" => "application/ocsp-request"
+        last_response.content_type.should == "application/ocsp-response"
+        last_response.should be_ok
+        ocsp_response = R509::Ocsp::Response.parse(last_response.body)
+        ocsp_response.status.should == OpenSSL::OCSP::RESPONSE_STATUS_SUCCESSFUL
+        ocsp_response.basic.status[0][1].should == OpenSSL::OCSP::V_CERTSTATUS_REVOKED
+        ocsp_response.basic.status[0][0].serial.should == 1051177536915098490149656742929223623669143613238
+    end
+    it "should return a valid (VALID) response on a POST request from the configured CA" do
+        class R509::Validity::Redis::Checker
+            def check(serial)
+                R509::Validity::Status.new(:status => R509::Validity::VALID, :revocation_time => nil, :revocation_reason => 0)
+            end
+        end
+        der = Base64.decode64(URI.decode("MFYwVDBSMFAwTjAJBgUrDgMCGgUABBQ4ykaMB0SN9IGWx21tTHBRnmCnvQQUeXW7hDrLLN56Cb4xG0O8HCpNU1gCFQC4IG5U4zC4RYb4VQ%2B2f0zCoFCvNg%3D%3D"))
+        post '/', der, "CONTENT_TYPE" => "application/ocsp-request"
+        last_response.content_type.should == "application/ocsp-response"
+        last_response.should be_ok
+        ocsp_response = R509::Ocsp::Response.parse(last_response.body)
+        ocsp_response.status.should == OpenSSL::OCSP::RESPONSE_STATUS_SUCCESSFUL
+        ocsp_response.basic.status[0][1].should == OpenSSL::OCSP::V_CERTSTATUS_GOOD
+        ocsp_response.basic.status[0][0].serial.should == 1051177536915098490149656742929223623669143613238
+    end
 end
